9f03b31e959e179c87f3ed9c046f28245404fc38
mehedihasanmehedi009 <mehedihasanmehedi007008@gmail.com>
2025-10-18 06:52:51 -0600

    mehedi

--- src/Components/catagorit.jsx
+++ src/Components/catagorit.jsx
@@ -1,28 +1,26 @@
-import React, { use } from 'react';
-import { NavLink } from 'react-router';
-const catagorispromise = fetch("/categories.json")
-.then((res)=> res.json()) 
+import React, { use } from "react";
+import { NavLink } from "react-router";
+const catagorispromise = fetch("/categories.json").then((res) => res.json());
 const Catagorit = () => {
-     const catagoris = use(catagorispromise)
-    return (
-        <div>
-            <h1 className='font-bold'>All Ctagores
-            </h1>  
-            <div className='grid  grid-col-1 mt-5'>
-                {
-                    catagoris.map(catagori=>
-                        <NavLink  key={catagori.id}  
-                        className={"btn bg-base-100 border-0 hover:bg-base-200  text-{20px} font-semibold"}
-                        to={`/Catagory/${catagori.id}`}
-
-                        >
-                            {catagori.name}
-                        </NavLink>
-                    )
-                }z
-            </div>
-        </div>
-    );
+  const catagoris = use(catagorispromise);
+  return (
+    <div>
+      <h1 className="font-bold">All Ctagores</h1>
+      <div className="grid  grid-col-1 mt-5">
+        {catagoris.map((catagori) => (
+          <NavLink
+            key={catagori.id}
+            className={
+              " btn bg-base-100 border-0 hover:bg-base-200  text-{20px} font-semibold"
+            }
+            to={`/Catagory/${catagori.id}`}
+          >
+            {catagori.name}
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Catagorit;
